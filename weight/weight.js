65f95df8e6f4e8dc37e0a3714a1bb0f0849279ce
Matt <mmeza84@gmail.com>
2020-12-25 00:51:32 -0600

    quick vue conversion for entry list

--- weight/weight.js
+++ weight/weight.js
@@ -1,84 +1,87 @@
-var loadedData;
-var chart;
+var LOCAL_STORAGE_KEY = 'weightchart.data';
 
-document.addEventListener('DOMContentLoaded', function() {
-	// fetch data
-	var cachedData = localStorage.getItem('weightchart.data');
-	if (!cachedData) {
-		//mock data
-		loadedData = [];
-	} else {
-		loadedData = JSON.parse(cachedData);
-	}
+var app = new Vue({
+	el: `#app`,
+	data: {
+		entries: [],
+		newDate: '',
+		newWeight: 0
+	},
+	created: function() {
+		var cachedData = localStorage.getItem(LOCAL_STORAGE_KEY);
+		if (!cachedData) {
+			this.entries = [];
+		} else {
+			this.entries = JSON.parse(cachedData);
+		}
+	},
+	methods: {
+		addWeightInput: function(event) {
+			if (this.newDate && this.newWeight) {
+				console.log('new date, new weight', this.newDate, this.newWeight);
+				
+				// push into data array
+				this.entries.push({
+					date: this.newDate,
+					weight: this.newWeight
+				});
 
-	// render chart
-	var ctx = document.getElementById('weightChart').getContext('2d');
-	chart = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: loadedData.map(function(entry) {
-				return entry.date;
-			}),
-			datasets: [{
-				label: 'Your Daily Weigh-In',
-				backgroundColor: 'rgb(54, 162, 235)',
-				borderColor: 'rgb(54, 162, 235)',
-				data: loadedData.map(function(entry) {
-					return entry.weight;
-				}),
-				fill: false
-			}]
-		},
-		options: {
-			title: {
-				display: true,
-				text: 'Weight Chart'
-			},
-			scales: {
-				xAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Date'
-					}
-				}],
-				yAxes: [{
-					display: true,
-					scaleLabel: {
-						display: true,
-						labelString: 'Weight'
-					}
-				}]
+				// save to localStorage
+				localStorage.removeItem(LOCAL_STORAGE_KEY);
+				localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.entries));
+
+				// push into live data
+				this.chart.data.labels.push(this.newDate);
+				this.chart.data.datasets[0].data.push(this.newWeight);
+				this.chart.update();
+
+				// clear values
+				this.newDate = '';
+				this.newWeight = 0;
 			}
 		}
-	});
-});
-
-function addWeightInput() {
-	var dateInput = document.getElementById('weighInDate');
-	var weightInput = document.getElementById('weighInValue');
-
-	if (dateInput.value && weightInput.value) {
-		var date = dateInput.value;
-		var weight = weightInput.value;
-
-		// push into data array
-		loadedData.push({
-			date,
-			weight
+	},
+	mounted: function() {
+		// load chart
+		var ctx = document.getElementById('weightChart').getContext('2d');
+		this.chart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: this.entries.map(function(entry) {
+					return entry.date;
+				}),
+				datasets: [{
+					label: 'Your Daily Weigh-In',
+					backgroundColor: 'rgb(54, 162, 235)',
+					borderColor: 'rgb(54, 162, 235)',
+					data: this.entries.map(function(entry) {
+						return entry.weight;
+					}),
+					fill: false
+				}]
+			},
+			options: {
+				title: {
+					display: true,
+					text: 'Weight Chart'
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Date'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Weight'
+						}
+					}]
+				}
+			}
 		});
-
-		// TODO save to localStorage
-
-		// push into live data
-		chart.data.labels.push(dateInput.value);
-		chart.data.datasets[0].data.push(weightInput.value);
-		chart.update();
-
-		// clear values
-		dateInput.value = '';
-		weightInput.value = '';
 	}
-
-	return false;
-}
+});
